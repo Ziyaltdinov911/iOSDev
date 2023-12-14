a0999a4997ecc462caec883f77fbc95ec7dec356
NSaveME <nsavekz@gmail.com>
2023-12-14 11:14:47 +0500

    Task-1

--- Project-HW/main.swift
+++ Project-HW/main.swift
@@ -7,5 +7,34 @@
 
 import Foundation
 
-print("Hello, World!")
+// Задание 1: Создание класса Car
+// Описание: Опишите класс "Car", включающий общие свойства автомобилей и пустой метод действия.
+// Требования:
+// Определите общие свойства для автомобилей (например, марка, модель, год выпуска и т. д.)
+// Включите пустой метод действия.
+// Подсказки:
+// Не забудьте использовать конструктор для инициализации свойств.
+//
+// Ожидаемый результат:
+// Класс Car, который содержит общие свойства автомобилей и пустой метод действия.
+//
 
+class Car {
+    var brand: String
+    var model: String
+    var year: Int
+    
+    init(brand: String, model: String, year: Int) {
+        self.brand = brand
+        self.model = model
+        self.year = year
+    }
+    
+    func movingCar() {
+        print("Машина \(brand) \(model) \(year) едет к тебе")
+    }
+}
+
+let aboutCar = Car(brand: "Jeep", model: "Grand Cheeroke", year: 2008)
+aboutCar.movingCar()
+ 
